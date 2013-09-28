e6fd87d5916c6bfa4c1d89a8aac2d9966312ee27
William Wu <williamwu.2k12@gmail.com>
2013-09-28 16:20:14 -0700

    some more things

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,4 +1,5 @@
-// canvas = new createjs.Stage("gameOutput");
+
+canvas = new createjs.Stage("gameOutput");
 
 function getObjects(array, property, value)
 {
@@ -15,6 +16,57 @@ function getObjects(array, property, value)
 
 
 
+/*
+function drawPlayers(context, array)
+{
+    var players = new getObjects(superObject, "type", "player");
+    var result = new Array();
+    for (i = 0; i < players.length; i = i + 1)
+    {
+        result.push(new createjs.Shape);
+    }
+}
+*/
+
+
+/*
+function triangle(context, position, size)
+{
+    context.beginPath();
+    if (position[2] == 0)
+    {
+        context.moveTo(position[0] + size, position[1])
+        context.lineTo();
+        context.lineTo();
+    }
+    if (position[2] == 2)
+    {
+        context.moveTo(position[0], position[1] + size)
+        context.lineTo();
+        context.lineTo();
+    }
+    if (position[2] == 4)
+    {
+        context.moveTo(position[0] - size, position[1])
+        context.lineTo();
+        context.lineTo();
+    }
+    if (position[2] == 6)
+    {
+        context.moveTo(position[0], position[1] - size)
+        context.lineTo();
+        context.lineTo();
+    }
+    context.lineTo();
+    context.lineTo();
+    context.fill();
+}
+
+function circle(context, position, size)
+{
+    context.arc(position[0], position[1], size, 0, 2 * Math.PI, true);
+}
+*/
 
 
 
@@ -28,26 +80,26 @@ var superObject =
     {
         type: "player",
         name: "william",
-        position: [6, 9],
+        position: [6, 9, 0],
         health: 69
     },
     {
         type: "projectile",
         name: "bullet",
-        position: [5, 10],
+        position: [5, 10, 1],
         health: undefined
     },
     {
         type: "projectile",
         name: "bullet",
-        position: [5, 10],
+        position: [5, 10, 2],
         health: undefined
     },
     {
-        type: "penis",
-        name: "vagina",
-        position: [],
-        health: 69
+        type: "obstacle",
+        name: "wall",
+        position: [169, 269, 3],
+        health: 100
     }
 ]
 
