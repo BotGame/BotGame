bc332eab16a8b58442532ae1dae57c9d2be1edb1
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-09-28 15:18:11 -0700

    nothing

--- assets/js/server.js
+++ assets/js/server.js
@@ -1,3 +1,34 @@
+//client
+
+function startListeningData()
+{
+	var socket = io.connect('http://localhost');
+	socket.on('gamestate', function (data)
+	{
+    	getObjects(data);
+    	//socket.emit('my other event', { my: 'data' });
+	});
+}
+
+function setUpCanvas()
+{
+/*
+	stage = new createjs.Stage("gameOutput");
+	createjs.Ticker.addEventListener("tick", handleTick);
+
+	function handleTick() {
+     //Circle will move 10 units to the right.
+        circle.x += 10;
+        //Will cause the circle to wrap back
+        if (circle.x > stage.canvas.width) { circle.x = 0; }
+        stage.update();
+    }
+*/
+}
+
+/*
+server
+
 var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
@@ -6,3 +37,4 @@ io.sockets.on('connection', function (socket) {
     console.log(data);
   });
 });
+*/
