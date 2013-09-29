ffdc13f172cf557e9a49af2619587ae9f152f7a7
Nitin Sadras <nitin.sadras@gmail.com>
2013-09-28 17:05:28 -0700

    interface between server and user-provided AI script

--- assets/js/interpreter.js
+++ assets/js/interpreter.js
@@ -1,17 +1,22 @@
-
+var socket = io.connect('http://'+window.location.host);
 
 function outf(text) { 
         var mypre = document.getElementById("output"); 
-            mypre.innerHTML = mypre.innerHTML + text; 
+        mypre.innerHTML = mypre.innerHTML + text;
+        //process text
+        var move = window.JSON.parse(text);
+        socket.emit('command',{sessionKey:mykey, move);
+        
 } 
 function builtinRead(x) {
         if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-                        throw "File not found: '" + x + "'";
-                            return Sk.builtinFiles["files"][x];
+            throw "File not found: '" + x + "'";
+        return Sk.builtinFiles["files"][x];
 }
 
-function runit() { 
-   var prog = document.getElementById("yourcode").value; 
+function runit(gamestate) { 
+   var prog = 'gameState='+gamestate+ "\n" + document.getElementById("yourcode").value + '\nprint getMove(gameState)';
+   console.log(prog);
    var mypre = document.getElementById("output"); 
    mypre.innerHTML = ''; 
    Sk.canvas = "mycanvas";
@@ -19,3 +24,10 @@ function runit() {
    Sk.configure({output:outf, read:builtinRead}); 
    eval(Sk.importMainWithBody("<stdin>",false,prog)); 
 } 
+
+function heartbeatListener(){
+    socket.on('game_heartbeat',function(gameState){
+        data = window.JSON.stringify(gameState);
+        runit(data);
+    });    
+}
